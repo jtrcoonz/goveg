230a9f9ca0b2d77e2d951c95ec5a7c844f029757
Jacob Coons <jacobtcoons@gmail.com>
2018-07-30 15:00:14 -0700

    Fixed recipe results, added functionality for recipe page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// const RECIPE_COMPLEX_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex";
+const RECIPE_COMPLEX_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex";
+let recipes = [];
 
 $('.search-form').submit(function (event) {
 	event.preventDefault();
@@ -8,7 +9,25 @@ $('.search-form').submit(function (event) {
 	getDataFromApi(query);
 })
 
-$('.search-result').click(function(event) {
+$('.search-results').on('click', '.result', function(event) {
+  let selectedIndex = $(event.currentTarget).attr('data-index');
+  let recipe = recipes[selectedIndex];
+  console.log(recipe);
+  $('.search-result-final').html(`
+    <h2 class="result-title">${recipe.title}</h2>
+    <div class="image-container-3">
+      <img src="img/veggie-burger-4.svg" alt="a veggie burger" class="result-image">
+    </div>
+    <div class="info-container-2">
+      <p class="info-label">Cuisine</p>
+      <p class="info">American</p>
+      <p class="info-label">Diet</p>
+      <p class="info">Vegan, vegetarian</p>
+      <p class="info-label">Prep time</p>
+      <p class="info">30 min.</p>
+    </div>
+    <div class="clear"></div>
+    `)
 	$('#page-2').hide();
 	$('#page-3').show();
 })
@@ -17,6 +36,9 @@ function getDataFromApi(query) {
   const settings = {
     url: RECIPE_COMPLEX_URL,
     data: {
+      fillIngredients: true,
+      instructionsRequired: true,
+      addRecipeInformation: true,
       query: query
     },
     headers: {
@@ -34,20 +56,27 @@ function getDataFromApi(query) {
 }
 
 function displayRecipeSearchData(data) {
-  console.log(data);
-  // const results = data.items.map((item, index) => renderResult(item));
-  // $('.js-search-results').html(results);
+  recipes = data.results;
+  const results = recipes.map((item, index) => renderResult(item, index));
+  $('.search-results').html(results);
 }
 
-function renderResult(result) {
-  console.log(result);
+function renderResult(result, index) {
+  // console.log(result);
+  const ingredients = result.missedIngredients.map((item, index) => item.name)
   return `
-  <div class="search-result">
-  <a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">
-    <h3>${result.snippet.title}</h3>
-    <img src="${result.snippet.thumbnails.medium.url}" class="yt-thumbnail">
-  </a>
-  <p>${result.snippet.description}</p>
-  </div>
+    <div data-index="${index}" class="result">
+      <h2 class="result-title">${result.title}</h2>
+        <div class="image-container-2">
+          <img src=${result.image} alt="search result image" class="result-image">
+        </div>
+        <div class="info-container">
+          <p><span class="info-label">Cuisine:</span> ${result.cuisines.join(', ')}</p>
+          <p><span class="info-label">Diet:</span> ${result.diets.join(', ')}</p>
+          <p><span class="info-label">Ingredients:</span> ${ingredients.join(', ')}</p>
+          <p><span class="info-label">Ready in:</span> ${result.readyInMinutes} min.</p>
+        </div>
+        <div class="clear"></div>
+    </div>
   `;
 }
